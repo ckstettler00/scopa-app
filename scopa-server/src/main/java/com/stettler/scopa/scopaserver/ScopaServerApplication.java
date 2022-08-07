abea92368e921c06291770b3f648b32992147023
Chip Stettler <robert.stettler@capitalone.com>
2022-08-06 23:15:55 -0400

    initial commit

---
+++ scopa-server/src/main/java/com/stettler/scopa/scopaserver/ScopaServerApplication.java
@@ -0,0 +1,13 @@
+package com.stettler.scopa.scopaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScopaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScopaServerApplication.class, args);
+	}
+
+}
