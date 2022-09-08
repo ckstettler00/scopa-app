35cae01f0ef2fbf6d42e78ceeb861dff285cf9bd
ckste <ckstettler@gmail.com>
2022-09-07 21:16:41 -0400

    Added port 8090 and enabled cors headers.

--- scopa-server/src/main/java/com/stettler/scopa/scopaserver/controller/ApiController.java
+++ scopa-server/src/main/java/com/stettler/scopa/scopaserver/controller/ApiController.java
@@ -6,14 +6,12 @@ import com.stettler.scopa.scopaserver.service.GameService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/scopa")
 public class ApiController {
     Logger logger = LoggerFactory.getLogger(getClass().getName());
